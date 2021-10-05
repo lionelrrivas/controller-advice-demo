15167e680a57cda6d6728c64d8c27c62a41c74e4
lionelrrivas <lionel.r.rivas@gmail.com>
2021-10-05 14:58:41 -0500

    remove unused import

--- src/main/java/com/demo/exception/advice/CustomerControllerAdvice.java
+++ src/main/java/com/demo/exception/advice/CustomerControllerAdvice.java
@@ -3,7 +3,6 @@ package com.demo.exception.advice;
 import javax.servlet.http.HttpServletRequest;
 import com.demo.entity.CustomerError;
 import com.demo.exception.CustomerNotFoundException;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +15,7 @@ public class CustomerControllerAdvice {
     @ExceptionHandler(CustomerNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     @ResponseBody
-    public CustomerError handleNotFound(HttpServletRequest request, HttpServletResponse response, Exception exception) {
+    public CustomerError handleNotFound(HttpServletRequest request, Exception exception) {
         return new CustomerError(exception.getLocalizedMessage(), request.getRequestURI());
     }
     
